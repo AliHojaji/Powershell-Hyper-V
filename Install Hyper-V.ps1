59a7c0e3c76ffde5b919382c815041b0553dd24b
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 12:45:51 +0330

    Add files via upload
    Install Hyper-V with Powershell

---
+++ Install Hyper-V.ps1
@@ -0,0 +1,34 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*----------------------*--#
+#---> HV1-Test (Desktop) <---#
+#--*----------------------*--#
+
+#--> install Hyper-V remotely
+Install-WindowsFeature -ComputerName HV1-TEST -Name Hyper-V IncludeManagementTools -Restart
+
+
+#--*-------------------*--#
+#---> HV2-TEST (Nano) <---#
+#--*-------------------*--#
+
+#--> enter remote session
+Enter-PSSession -ComputerName HV2-TEST
+
+#--> import provider, view installed and available packages
+Import-PackageProvider -Name NanoServerPackage
+Get-Package -ProviderName NanoServerPackage
+Find-Package -ProviderName NanoServerPackage
+
+#--> install the hyper-v role
+Install-NanoServerPackage -Name Microsoft-NanoServer-Compute-Package
+
+#reboot
+Restart-Computer
+
+
+#--> ADMIN-TEST (Local) <--#
+
+#--> install GUIm cndk
+Get-WindowsOptionalFeature -Online -FeatureName *Hyper-V*
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Tools-All
