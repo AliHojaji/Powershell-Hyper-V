59e49a7cdc4a9d9abf578776e4619a2d757562eb
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 12:56:02 +0330

    Add files via upload
    Configure Hyper-V Networking with Powershell

---
+++ Configure Hyper-V Networking.ps1
@@ -0,0 +1,23 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*--------------------------------*--#
+#---> Configure Hyper-V Networking <---#
+#--*--------------------------------*--#
+
+#--> remote over to HV1-TEST
+Enter-PSSession -ComputerName HV1-TEST
+
+#--> create a switch
+New-VMSwitch -SwitchName vInternal -SwitchType Internal
+
+#--> configure switch IP (gateway)
+New-NetIPAddress -IPAddress 10.10.0.1 -PrefixLength 24 -InterfaceAlias "vEthernet (vInternal)"
+
+#--> configure network address translation
+New-NetNAT -Name "vNAT" -InternalIPInterfaceAddressPrefix 10.10.0.0/24
+
+#--> add a virtual NIC
+Add-NetEventVmNetworkAdapter -VMName CORE-TEST -SwitchName vInternal
+
+#--> exit remote session 
+Exit-PSSession
